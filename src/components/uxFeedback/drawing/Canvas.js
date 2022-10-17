dd2f28920a659671d5fdc5ac39ea9ae0ec5e2f45
Manoj Gamachchige <manojpriyankaraucsc@gmail.com>
2022-10-17 23:00:05 +0200

    temp commit with new canvas

--- src/components/uxFeedback/drawing/Canvas.js
+++ src/components/uxFeedback/drawing/Canvas.js
@@ -1,124 +1,522 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import rough from "roughjs/bundled/rough.esm";
+import getStroke from "perfect-freehand";
 
 import "./Canvas.css";
 
-const Canvas = ({ activeTool }) => {
-  const canvasRef = useRef(null);
-  const contextRef = useRef(null);
-  const canvasOffsetX = useRef(null);
-  const canvasOffsetY = useRef(null);
-  const startX = useRef(null);
-  const startY = useRef(null);
+import PanToolIcon from "@mui/icons-material/PanTool";
+import CreateIcon from "@mui/icons-material/Create";
+import CropSquareIcon from "@mui/icons-material/CropSquare";
+import CommentIcon from "@mui/icons-material/Comment";
+import RedoIcon from "@mui/icons-material/Redo";
+import UndoIcon from "@mui/icons-material/Undo";
 
-  const [isDrawing, setIsDrawing] = useState(false);
+const generator = rough.generator();
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    canvas.width = window.innerWidth * 2;
-    canvas.height = window.innerHeight * 2;
-    canvas.style.width = `${window.innerWidth}px`;
-    canvas.style.height = `${window.innerHeight}px`;
+const createElement = (id, x1, y1, x2, y2, type, options) => {
+  switch (type) {
+    case "line":
+    case "rectangle":
+      const roughElement =
+        type === "line"
+          ? generator.line(x1, y1, x2, y2, { stroke: options.color })
+          : generator.rectangle(x1, y1, x2 - x1, y2 - y1, {
+              stroke: options.color,
+            });
+      return { id, x1, y1, x2, y2, type, options, roughElement };
+    case "pencil":
+      return { id, type, points: [{ x: x1, y: y1 }], options };
+    case "text":
+      return { id, type, x1, y1, x2, y2, text: "" };
+    default:
+      throw new Error(`Type not recognised: ${type}`);
+  }
+};
 
-    const context = canvas.getContext("2d");
-    context.scale(2, 2);
-    context.lineCap = "round";
-    context.strokeStyle = "red";
-    context.lineWidth = 2;
-    contextRef.current = context;
+const nearPoint = (x, y, x1, y1, name) => {
+  return Math.abs(x - x1) < 5 && Math.abs(y - y1) < 5 ? name : null;
+};
 
-    const canvasOffSet = canvas.getBoundingClientRect();
-    canvasOffsetX.current = canvasOffSet.top;
-    canvasOffsetY.current = canvasOffSet.left;
-  }, []);
+const onLine = (x1, y1, x2, y2, x, y, maxDistance = 1) => {
+  const a = { x: x1, y: y1 };
+  const b = { x: x2, y: y2 };
+  const c = { x, y };
+  const offset = distance(a, b) - (distance(a, c) + distance(b, c));
+  return Math.abs(offset) < maxDistance ? "inside" : null;
+};
 
-  useEffect(() => {
-    if (activeTool === "eraser") {
-      contextRef.current.globalCompositeOperation = "destination-out";
-      contextRef.current.lineWidth = 50;
+const positionWithinElement = (x, y, element) => {
+  const { type, x1, x2, y1, y2 } = element;
+  switch (type) {
+    case "line":
+      const on = onLine(x1, y1, x2, y2, x, y);
+      const start = nearPoint(x, y, x1, y1, "start");
+      const end = nearPoint(x, y, x2, y2, "end");
+      return start || end || on;
+    case "rectangle":
+      const topLeft = nearPoint(x, y, x1, y1, "tl");
+      const topRight = nearPoint(x, y, x2, y1, "tr");
+      const bottomLeft = nearPoint(x, y, x1, y2, "bl");
+      const bottomRight = nearPoint(x, y, x2, y2, "br");
+      const inside = x >= x1 && x <= x2 && y >= y1 && y <= y2 ? "inside" : null;
+      return topLeft || topRight || bottomLeft || bottomRight || inside;
+    case "pencil":
+      const betweenAnyPoint = element.points.some((point, index) => {
+        const nextPoint = element.points[index + 1];
+        if (!nextPoint) return false;
+        return (
+          onLine(point.x, point.y, nextPoint.x, nextPoint.y, x, y, 5) != null
+        );
+      });
+      return betweenAnyPoint ? "inside" : null;
+    case "text":
+      return x >= x1 && x <= x2 && y >= y1 && y <= y2 ? "inside" : null;
+    default:
+      throw new Error(`Type not recognised: ${type}`);
+  }
+};
+
+const distance = (a, b) =>
+  Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+
+const getElementAtPosition = (x, y, elements) => {
+  return elements
+    .map((element) => ({
+      ...element,
+      position: positionWithinElement(x, y, element),
+    }))
+    .find((element) => element.position !== null);
+};
+
+const adjustElementCoordinates = (element) => {
+  const { type, x1, y1, x2, y2 } = element;
+  if (type === "rectangle") {
+    const minX = Math.min(x1, x2);
+    const maxX = Math.max(x1, x2);
+    const minY = Math.min(y1, y2);
+    const maxY = Math.max(y1, y2);
+    return { x1: minX, y1: minY, x2: maxX, y2: maxY };
+  } else {
+    if (x1 < x2 || (x1 === x2 && y1 < y2)) {
+      return { x1, y1, x2, y2 };
     } else {
-      contextRef.current.globalCompositeOperation = "source-over";
-      contextRef.current.lineWidth = 2;
+      return { x1: x2, y1: y2, x2: x1, y2: y1 };
     }
-  }, [activeTool]);
+  }
+};
 
-  const startDrawing = ({ nativeEvent }) => {
-    const { offsetX, offsetY } = nativeEvent;
+const cursorForPosition = (position) => {
+  switch (position) {
+    case "tl":
+    case "br":
+    case "start":
+    case "end":
+      return "nwse-resize";
+    case "tr":
+    case "bl":
+      return "nesw-resize";
+    default:
+      return "move";
+  }
+};
 
-    contextRef.current.beginPath();
-    contextRef.current.moveTo(offsetX, offsetY);
-    contextRef.current.lineTo(offsetX, offsetY);
-    contextRef.current.stroke();
-    setIsDrawing(true);
-    nativeEvent.preventDefault();
-  };
+const resizedCoordinates = (clientX, clientY, position, coordinates) => {
+  const { x1, y1, x2, y2 } = coordinates;
+  switch (position) {
+    case "tl":
+    case "start":
+      return { x1: clientX, y1: clientY, x2, y2 };
+    case "tr":
+      return { x1, y1: clientY, x2: clientX, y2 };
+    case "bl":
+      return { x1: clientX, y1, x2, y2: clientY };
+    case "br":
+    case "end":
+      return { x1, y1, x2: clientX, y2: clientY };
+    default:
+      return null; //should not really get here...
+  }
+};
 
-  const draw = ({ nativeEvent }) => {
-    if (!isDrawing) {
-      return;
+const useHistory = (initialState) => {
+  const [index, setIndex] = useState(0);
+  const [history, setHistory] = useState([initialState]);
+
+  const setState = (action, overwrite = false) => {
+    const newState =
+      typeof action === "function" ? action(history[index]) : action;
+    if (overwrite) {
+      const historyCopy = [...history];
+      historyCopy[index] = newState;
+      setHistory(historyCopy);
+    } else {
+      const updatedState = [...history].slice(0, index + 1);
+      setHistory([...updatedState, newState]);
+      setIndex((prevState) => prevState + 1);
     }
-    const { offsetX, offsetY } = nativeEvent;
-    contextRef.current.lineTo(offsetX, offsetY);
-    contextRef.current.stroke();
-    nativeEvent.preventDefault();
   };
 
-  const stopDrawing = () => {
-    contextRef.current.closePath();
-    setIsDrawing(false);
-  };
+  const undo = () => index > 0 && setIndex((prevState) => prevState - 1);
+  const redo = () =>
+    index < history.length - 1 && setIndex((prevState) => prevState + 1);
 
-  const startDrawingRectangle = ({ nativeEvent }) => {
-    nativeEvent.preventDefault();
-    nativeEvent.stopPropagation();
+  return [history[index], setState, undo, redo];
+};
 
-    startX.current = nativeEvent.clientX - canvasOffsetX.current;
-    startY.current = nativeEvent.clientY - canvasOffsetY.current;
+const getSvgPathFromStroke = (stroke) => {
+  if (!stroke.length) return "";
 
-    setIsDrawing(true);
-  };
+  const d = stroke.reduce(
+    (acc, [x0, y0], i, arr) => {
+      const [x1, y1] = arr[(i + 1) % arr.length];
+      acc.push(x0, y0, (x0 + x1) / 2, (y0 + y1) / 2);
+      return acc;
+    },
+    ["M", ...stroke[0], "Q"]
+  );
 
-  const drawingRectangle = ({ nativeEvent }) => {
-    if (!isDrawing) {
-      return;
+  d.push("Z");
+  return d.join(" ");
+};
+
+const drawElement = (roughCanvas, context, element) => {
+  switch (element.type) {
+    case "line":
+    case "rectangle":
+      context.strokeStyle = "red";
+      roughCanvas.draw(element.roughElement);
+      break;
+    case "pencil":
+      context.fillStyle = element.options.color;
+      const stroke = getSvgPathFromStroke(getStroke(element.points));
+      context.fill(new Path2D(stroke));
+      break;
+    case "text":
+      context.textBaseline = "top";
+      context.font = "24px sans-serif";
+      context.fillText(element.text, element.x1, element.y1);
+      break;
+    default:
+      throw new Error(`Type not recognised: ${element.type}`);
+  }
+};
+
+const adjustmentRequired = (type) => ["line", "rectangle"].includes(type);
+
+const Canvas = () => {
+  const [elements, setElements, undo, redo] = useHistory([]);
+  const [action, setAction] = useState("none");
+  const [selectedElement, setSelectedElement] = useState(null);
+  const [tool, setTool] = useState("pencil");
+  const color = "red";
+  const textAreaRef = useRef();
+
+  useLayoutEffect(() => {
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    const roughCanvas = rough.canvas(canvas);
+
+    elements.forEach((element) => {
+      if (action === "writing" && selectedElement.id === element.id) return;
+      drawElement(roughCanvas, context, element);
+    });
+  }, [elements, action, selectedElement]);
+
+  useEffect(() => {
+    const undoRedoFunction = (event) => {
+      if ((event.metaKey || event.ctrlKey) && event.key === "z") {
+        if (event.shiftKey) {
+          redo();
+        } else {
+          undo();
+        }
+      }
+    };
+
+    document.addEventListener("keydown", undoRedoFunction);
+    return () => {
+      document.removeEventListener("keydown", undoRedoFunction);
+    };
+  }, [undo, redo]);
+
+  useEffect(() => {
+    const textArea = textAreaRef.current;
+    if (action === "writing") {
+      // textArea.focus();
+      textArea.value = selectedElement.text;
     }
-    nativeEvent.preventDefault();
-    nativeEvent.stopPropagation();
+  }, [action, selectedElement]);
 
-    const newMouseX = nativeEvent.clientX - canvasOffsetX.current;
-    const newMouseY = nativeEvent.clientY - canvasOffsetY.current;
+  const updateElement = (id, x1, y1, x2, y2, type, options) => {
+    const elementsCopy = [...elements];
 
-    const rectWidth = newMouseX - startX.current;
-    const rectHeight = newMouseY - startY.current;
+    switch (type) {
+      case "line":
+      case "rectangle":
+        elementsCopy[id] = createElement(id, x1, y1, x2, y2, type, options);
+        break;
+      case "pencil":
+        elementsCopy[id].points = [
+          ...elementsCopy[id].points,
+          { x: x2, y: y2 },
+        ];
+        break;
+      case "text":
+        const textWidth = document
+          .getElementById("canvas")
+          .getContext("2d")
+          .measureText(options.text).width;
+        const textHeight = 24;
+        elementsCopy[id] = {
+          ...createElement(
+            id,
+            x1,
+            y1,
+            x1 + textWidth,
+            y1 + textHeight,
+            type,
+            options
+          ),
+          text: options.text,
+        };
+        break;
+      default:
+        throw new Error(`Type not recognised: ${type}`);
+    }
 
-    contextRef.current.clearRect(
-      startX.current - 2,
-      startY.current - 2,
-      canvasRef.current.width + 2,
-      canvasRef.current.height + 2
-    );
-
-    contextRef.current.strokeRect(
-      startX.current,
-      startY.current,
-      rectWidth,
-      rectHeight
-    );
+    setElements(elementsCopy, true);
   };
 
-  const stoptDrawingRectangle = ({ nativeEvent }) => {
-    setIsDrawing(false);
+  const handleMouseDown = (event) => {
+    if (action === "writing") return;
+
+    const { clientX, clientY } = event;
+    if (tool === "selection") {
+      const element = getElementAtPosition(clientX, clientY, elements);
+      if (element) {
+        if (element.type === "pencil") {
+          const xOffsets = element.points.map((point) => clientX - point.x);
+          const yOffsets = element.points.map((point) => clientY - point.y);
+          setSelectedElement({ ...element, xOffsets, yOffsets });
+        } else {
+          const offsetX = clientX - element.x1;
+          const offsetY = clientY - element.y1;
+          setSelectedElement({ ...element, offsetX, offsetY });
+        }
+        setElements((prevState) => prevState);
+
+        if (element.position === "inside") {
+          setAction("moving");
+        } else {
+          setAction("resizing");
+        }
+      }
+    } else {
+      const id = elements.length;
+      const element = createElement(
+        id,
+        clientX,
+        clientY,
+        clientX,
+        clientY,
+        tool,
+        { color }
+      );
+      setElements((prevState) => [...prevState, element]);
+      setSelectedElement(element);
+
+      setAction(tool === "text" ? "writing" : "drawing");
+    }
   };
 
-  console.log(activeTool);
+  const handleMouseMove = (event) => {
+    const { clientX, clientY } = event;
+
+    if (tool === "selection") {
+      const element = getElementAtPosition(clientX, clientY, elements);
+      event.target.style.cursor = element
+        ? cursorForPosition(element.position)
+        : "default";
+    }
+
+    if (action === "drawing") {
+      const index = elements.length - 1;
+      const { x1, y1, options } = elements[index];
+      updateElement(index, x1, y1, clientX, clientY, tool, options);
+    } else if (action === "moving") {
+      if (selectedElement.type === "pencil") {
+        const newPoints = selectedElement.points.map((_, index) => ({
+          x: clientX - selectedElement.xOffsets[index],
+          y: clientY - selectedElement.yOffsets[index],
+        }));
+        const elementsCopy = [...elements];
+        elementsCopy[selectedElement.id] = {
+          ...elementsCopy[selectedElement.id],
+          points: newPoints,
+        };
+        setElements(elementsCopy, true);
+      } else {
+        const {
+          id,
+          x1,
+          x2,
+          y1,
+          y2,
+          options: colorOptions,
+          type,
+          offsetX,
+          offsetY,
+        } = selectedElement;
+        const width = x2 - x1;
+        const height = y2 - y1;
+        const newX1 = clientX - offsetX;
+        const newY1 = clientY - offsetY;
+        const options = type === "text" ? { text: selectedElement.text } : {};
+        updateElement(id, newX1, newY1, newX1 + width, newY1 + height, type, {
+          ...options,
+          ...colorOptions,
+        });
+      }
+    } else if (action === "resizing") {
+      const { id, type, options, position, ...coordinates } = selectedElement;
+      const { x1, y1, x2, y2 } = resizedCoordinates(
+        clientX,
+        clientY,
+        position,
+        coordinates
+      );
+      updateElement(id, x1, y1, x2, y2, type, options);
+    }
+  };
+
+  const handleMouseUp = (event) => {
+    const { clientX, clientY } = event;
+    if (selectedElement) {
+      if (
+        selectedElement.type === "text" &&
+        clientX - selectedElement.offsetX === selectedElement.x1 &&
+        clientY - selectedElement.offsetY === selectedElement.y1
+      ) {
+        setAction("writing");
+        return;
+      }
+
+      const index = selectedElement.id;
+      const { id, type, options } = elements[index];
+      if (
+        (action === "drawing" || action === "resizing") &&
+        adjustmentRequired(type)
+      ) {
+        const { x1, y1, x2, y2 } = adjustElementCoordinates(elements[index]);
+        updateElement(id, x1, y1, x2, y2, type, options);
+      }
+    }
+
+    if (action === "writing") return;
+
+    setAction("none");
+    setSelectedElement(null);
+  };
+
+  const handleBlur = (event) => {
+    const { id, x1, y1, type } = selectedElement;
+    setAction("none");
+    setSelectedElement(null);
+    updateElement(id, x1, y1, null, null, type, { text: event.target.value });
+  };
+
   return (
-    <canvas
-      className="canvas"
-      ref={canvasRef}
-      onMouseDown={activeTool === "rect" ? startDrawingRectangle : startDrawing}
-      onMouseMove={activeTool === "rect" ? drawingRectangle : draw}
-      onMouseUp={activeTool === "rect" ? stoptDrawingRectangle : stopDrawing}
-      onMouseLeave={activeTool === "rect" ? stoptDrawingRectangle : stopDrawing}
-    />
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      <div
+        style={{
+          backgroundColor: "#9FC540",
+          padding: 10,
+          paddingTop: 20,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "flex-start",
+        }}
+      >
+        <div style={{ marginBottom: 20, display: "flex" }}>
+          <CreateIcon
+            style={{ width: 28, height: 28, cursor: "pointer" }}
+            sx={{ color: tool === "pencil" ? "white" : "black" }}
+            onClick={() => setTool("pencil")}
+          />
+        </div>
+        <div style={{ marginBottom: 20, display: "flex" }}>
+          <CropSquareIcon
+            style={{ width: 28, height: 28, cursor: "pointer" }}
+            sx={{ color: tool === "rectangle" ? "white" : "black" }}
+            onClick={() => setTool("rectangle")}
+          />
+        </div>
+
+        <div style={{ marginBottom: 20, display: "flex" }}>
+          <CommentIcon
+            style={{ width: 28, height: 28, cursor: "pointer" }}
+            sx={{ color: tool === "text" ? "white" : "black" }}
+            onClick={() => setTool("text")}
+          />
+        </div>
+        <div style={{ marginBottom: 20, display: "flex" }}>
+          <PanToolIcon
+            style={{ width: 28, height: 28, cursor: "pointer" }}
+            sx={{ color: tool === "selection" ? "white" : "black" }}
+            onClick={() => setTool("selection")}
+          />
+        </div>
+        <div style={{ marginBottom: 20, display: "flex" }}>
+          <UndoIcon
+            style={{ width: 28, height: 28, cursor: "pointer" }}
+            sx={{ color: "black" }}
+            onClick={undo}
+          />
+        </div>
+        <div style={{ marginBottom: 20, display: "flex" }}>
+          <RedoIcon
+            style={{ width: 28, height: 28, cursor: "pointer" }}
+            sx={{ color: "black" }}
+            onClick={redo}
+          />
+        </div>
+      </div>
+
+      {action === "writing" ? (
+        <textarea
+          ref={textAreaRef}
+          onBlur={handleBlur}
+          style={{
+            position: "fixed",
+            top: selectedElement.y1 - 2,
+            left: selectedElement.x1,
+            font: "24px sans-serif",
+            margin: 0,
+            padding: 0,
+            border: 0,
+            outline: 0,
+            resize: "auto",
+            overflow: "hidden",
+            whiteSpace: "pre",
+            background: "#9FC540",
+          }}
+        />
+      ) : null}
+      <canvas
+        id="canvas"
+        width={window.innerWidth}
+        height={window.innerHeight}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        className="canvas"
+      >
+        Canvas
+      </canvas>
+    </div>
   );
 };
 
--- src/components/uxFeedback/screenCaptureLayer/ScreenCaptureLayer.component.js
+++ src/components/uxFeedback/screenCaptureLayer/ScreenCaptureLayer.component.js
@@ -12,92 +12,25 @@ import commentSVG from "../images/comment.svg";
 import "./ScreenCaptureLayer.css";
 
 const ScreenCaptureLayer = ({ onClickCancel, onClickSave }) => {
-  const [activeTool, setActiveTool] = useState(null);
+  const [activeTool, setActiveTool] = useState("text");
 
   const handleClickTool = (tool) => {
     setActiveTool(tool);
   };
 
-  const handleCaptureScree = () => {
-    let node = document.getElementById("root");
-    htmlToImage
-      .toPng(node)
-      .then(function (dataUrl) {
-        onClickSave(dataUrl);
-      })
-      .catch(function (error) {
-        console.error("oops, something went wrong!", error);
-      });
-  };
+  // const handleCaptureScree = () => {
+  //   let node = document.getElementById("root");
+  //   htmlToImage
+  //     .toPng(node)
+  //     .then(function (dataUrl) {
+  //       onClickSave(dataUrl);
+  //     })
+  //     .catch(function (error) {
+  //       console.error("oops, something went wrong!", error);
+  //     });
+  // };
 
-  return (
-    <>
-      <Canvas activeTool={activeTool} />
-      <div className="screen-capture-layer">
-        <div className="screen-capture-layer-tool-pannel">
-          <div
-            className={classNames(
-              "tool-button-wrapper",
-              activeTool === "pen" && "tool-button-wrapper-active"
-            )}
-          >
-            <img
-              src={PenSVG}
-              alt="pen"
-              className="tool-svg"
-              onClick={() => handleClickTool("pen")}
-            />
-          </div>
-          <div
-            className={classNames(
-              "tool-button-wrapper",
-              activeTool === "eraser" && "tool-button-wrapper-active"
-            )}
-          >
-            <img
-              src={eraserSVG}
-              alt="pen"
-              className="tool-svg"
-              onClick={() => handleClickTool("eraser")}
-            />
-          </div>
-
-          <div
-            className={classNames(
-              "tool-button-wrapper",
-              activeTool === "rect" && "tool-button-wrapper-active"
-            )}
-          >
-            <img
-              src={rectangleSVG}
-              alt="pen"
-              className="tool-svg"
-              onClick={() => handleClickTool("rect")}
-            />
-          </div>
-          <div
-            className={classNames(
-              "tool-button-wrapper",
-              activeTool === "comment" && "tool-button-wrapper-active"
-            )}
-          >
-            <img src={commentSVG} alt="pen" className="tool-svg" />
-          </div>
-        </div>
-        {/* <div className="screen-capture-layer-button-pannel">
-          <div className="screen-capture-layer-cancel" onClick={onClickCancel}>
-            Cancel
-          </div>
-          <div
-            className="screen-capture-layer-save"
-            onClick={handleCaptureScree}
-          >
-            Save
-          </div>
-        </div> */}
-      </div>
-    </>
-  );
+  return <Canvas tool={activeTool} />;
 };
 
 export default ScreenCaptureLayer;
